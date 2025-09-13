838af941d2f8f88079f1e76b6f98f1f8c1af7ffc
Savannah Sawyer <savannahcoker3@gmail.com>
2025-09-12 22:35:22 -0500

    ready to publish no nodes nor src

--- src/App.jsx
+++
@@ -1,3 +0,0 @@
-// App.jsx removed - project converted to static HTML
-// Keep this placeholder to indicate the React app was intentionally removed.
-export default null;
--- src/main.jsx
+++
@@ -1,2 +0,0 @@
-// removed React entry point - project converted to static HTML
-// Original file was removed as part of switch to static site. Serve the root folder instead.
